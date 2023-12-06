db8bfcb5a92ec9b88282329c11c2cc2f4cf546c7
Michael Mwangi <michaelmichire2016@gmail.com>
2023-12-06 07:10:29 -0800

    A functions that print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,7 +1,6 @@
 #include <Python.h>
 
-void print_python_list(PyObject *p)
-void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
 
 /**
  * print_python_list - prints the basic information abt a python ls
