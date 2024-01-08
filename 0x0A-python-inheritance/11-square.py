8f56ac8c1a9881d7bee665c6eb965dd3673d80dd
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-08 11:00:13 -0800

    A class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,12 +3,60 @@
 """7-base_geometry module"""
 
 
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value to be a positive int"""
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class, inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Instantiates a Rectangle with given width and height"""
+
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates and returns the area of the rectangle"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle"""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
 class Square(Rectangle):
     """Square class, inherits from Rectangle"""
 
     def __init__(self, size):
         """Instantiates a Square with given size"""
 
+        self.__size = 0
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+        def __str__(self):
+            """Returns a string reps of the square"""
+
+            return f"[Square] {self.__size}"
