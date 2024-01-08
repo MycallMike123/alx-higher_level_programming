f5aad7c0ab58216712c1096aa6959b6924d32864
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-08 10:31:15 -0800

    A class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""7-base_geometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value to be a positive int"""
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class, inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Instantiates a Rectangle with given width and height"""
+
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates and returns the area of the rectangle"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle"""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
