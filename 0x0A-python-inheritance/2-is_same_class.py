1a1b0844c3f2501295ea0e45c2c4554513a5be67
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-08 07:47:02 -0800

    A function that returns True if the object is exactly an instance of the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Module with a class MyList"""
+
+
+class MyList(list):
+    """A custom list class that inherits from the built-in list class"""
+
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+
+        sorted_list = sorted(self)
+        print(sorted_list)
