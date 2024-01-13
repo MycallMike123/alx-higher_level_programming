bbe46a09f45d981e62a45c287f0e9cea58756751
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-13 04:45:03 -0800

    Handles mod Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""Defines a square class."""
+"""Module documentation explaining the purpose of models.square"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """Square class that inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
+        """Constructor/Initializer for the Square class"""
 
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
-        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Get/set the size of the Square."""
+        """Getter for size"""
+
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter for size"""
+
         self.width = value
         self.height = value
 
