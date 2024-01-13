c9518b51ddc0b031b24167832c0ed68ba3a6b63c
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-13 04:55:04 -0800

    Handles base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,19 +16,18 @@ class Base:
 
         if id is not None:
             self.id = id
+
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts.
+        """Return the JSON string representation of list_dictionaries"""
 
-        Args:
-            list_dictionaries (list): A list of dictionaries.
-        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
+
         return json.dumps(list_dictionaries)
 
     @classmethod
