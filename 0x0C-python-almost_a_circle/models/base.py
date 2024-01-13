da705caf6221b5292569676e636e02a268920530
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-13 05:01:53 -0800

    Handles base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,30 +46,24 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """Return the deserialization of a JSON string.
+        """Return the list of dictionaries from the JSON string rep"""
 
-        Args:
-            json_string (str): A JSON str representation of a list of dicts.
-        Returns:
-            If json_string is None or empty - an empty list.
-            Otherwise - the Python list represented by json_string.
-        """
         if json_string is None or json_string == "[]":
             return []
+
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Return a class instantied from a dictionary of attributes.
+        """Return aninstance with attributes set based on the provided dict"""
 
-        Args:
-            **dictionary (dict): Key/value pairs of attributes to initialize.
-        """
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new = cls(1, 1)
+
             else:
                 new = cls(1)
+
             new.update(**dictionary)
             return new
 
