f39a5378e01c803871b63c9d6ed7571125ca6eef
Michael Mwangi <michaelmichire2016@gmail.com>
2024-01-13 04:03:24 -0800

    Handles the base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
+"""Module documentation explaining the purpose of models.base"""
 
-"""Defines a base model class."""
 import json
 import csv
 import turtle
 
 
 class Base:
-    """Base model.
-
-    This Represents the "base" for all other classes in project 0x0C*.
-
-    Private Class Attributes:
-        __nb_object (int): Number of instantiated Bases.
-    """
+    """Base class for managing id attribute in all future classes"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
+        """Constructor/Initializes for the Base class"""
 
-        Args:
-            id (int): The identity of the new Base.
-        """
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
