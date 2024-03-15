83202bdd70a1dd1dffd5470bb93f325f51f9e010
Michael Mwangi <michaelmichire2016@gmail.com>
2024-03-15 05:37:14 -0700

    A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,13 +7,13 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    _cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
+    _cursor = db.cursor()
+    _cursor.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
 
-    r = cur.fetchall()
+    r = _cursor.fetchall()
     for row in r:
-
         print(row)
-    cur.close()
+
+    _cursor.close()
     db.close()
