fda49fff60deb4bbf8ff15809f812a29d7dcbbb7
Michael Mwangi <michaelmichire2016@gmail.com>
2024-03-15 07:56:42 -0700

    A script takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""This script takes in an argument and displays all values in the states table of hbtn_0e_0_usa"""
+"""This script takes in an argument and displays all
+3-my_safe_filter_states values in the states table of hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
