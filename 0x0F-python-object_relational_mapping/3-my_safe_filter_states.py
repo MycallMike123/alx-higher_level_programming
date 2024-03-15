a2d35b27ba71f550d49494b76d055174dc47b5e8
Michael Mwangi <michaelmichire2016@gmail.com>
2024-03-15 07:39:49 -0700

    A script takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-"""This script takes in an argument and displays all values in the states table of hbtn_0e_0_usa"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -9,14 +7,11 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
-    _cursor = db.cursor()
+    cur = db.cursor()
     match = sys.argv[4]
-    _cursor.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
-
-    r = _cursor.fetchall()
-    for row in r:
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    _cursor.close()
+    cur.close()
     db.close()
