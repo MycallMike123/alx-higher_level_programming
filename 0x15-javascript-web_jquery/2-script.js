f639126ec6900b2ae73ba8e135dabeaeec573ac9
Michael Mwangi <michaelmichire2016@gmail.com>
2024-05-02 10:21:51 -0700

    A avaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header, can’t use document.querySelector to select the HTML tag, must use the JQuery API

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Event listener for clicks on the div with id red_header
+$('DIV#red_header').click(function () {
+	// Select the <header> element using jQuery and update the text color to red
+  $('header').css('color', '#FF0000');
+});
